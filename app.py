6f03291667c1d35ca0682d24efbe9bc7caa46ac9
James Boon <jwboon@pm.me>
2023-03-29 13:14:50 +1000

    Example API end point is now working

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 
 import json
-from flask import Flask
+from flask import Flask, request
 
 
 app = Flask(__name__)
@@ -9,6 +9,24 @@ app = Flask(__name__)
 def index():
     return json.dumps({'name': 'test message',
                        'message': 'The API is working'})
+
+@app.route('/example', methods=['GET','POST'])
+def example_end_point():
+    if request.method == 'GET':
+        print("GET")
+        return json.dumps({'name': 'test message',
+                       'message': 'The API is working'})
+
+    if request.method == 'POST':
+        content_type = request.headers.get('Content-Type')
+        if (content_type == 'application/json'):
+            json_message = request.json
+            for m in json_message:
+                print(m)
+            return json_message
+        else:
+            return json.dumps({'ERROR': 'Error',
+                        'message': 'Content is not supported'})
 
 if __name__ == '__main__':
     app.run(port=8080)
