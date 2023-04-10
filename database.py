4a51ee928f96691e2b056d9f3c4eb463f7132c0b
James Boon <jwboon@pm.me>
2023-04-10 20:38:23 +1000

    Can store and retrieve fare data from database

--- database.py
+++ database.py
@@ -2,12 +2,15 @@ from sqlite3 import *
 from os import path
 import bcrypt
 
-def create_user_database(db_name):
+db_file = "./Storage.db"
+
+def create_database(db_name):
 
     connection = connect(database=db_name)
     db = connection.cursor()
 
     db.execute("CREATE TABLE Users(UserId INTEGER UNIQUE primary key AUTOINCREMENT, UserName TEXT UNIQUE, UserPassword TEXT(200), Name TEXT, UserType INTEGER);")
+    db.execute("CREATE TABLE BookingData(Year INTEGER, Day INTEGER, BaseFare FLOAT, TotalSpaces INTEGER, SpacesSold INTEGER, DaysForSale INTEGER, CONSTRAINT  PKYearDay Primary Key (Year, Day));")
 
     connection.commit()
 
@@ -17,11 +20,11 @@ def create_user_database(db_name):
 def add_user_to_database(user_name, user_password):
 
     user_password_hashed = str(hash_password(user_password))
-    users_db_file = "./Users.db"
-    if not path.exists(users_db_file):
-        create_user_database(users_db_file)
+    global db_file
+    if not path.exists(db_file):
+        create_database(db_file)
     
-    connection = connect(database=users_db_file)
+    connection = connect(database=db_file)
     db = connection.cursor()
 
     sql_instruction = f"INSERT INTO Users(UserName, UserPassword) VALUES ('{user_name}', '{user_password_hashed}');"
@@ -56,13 +59,13 @@ def check_hash(entered_password, hashed_password):
     return result
 
 def check_user_password_in_database(entered_user_name, entered_user_password):
-    users_db_file = "./Users.db"
+    global db_file
 
-    if not path.exists(users_db_file):
+    if not path.exists(db_file):
         print("Database does not exist")
         return False
     
-    connection = connect(database=users_db_file)
+    connection = connect(database=db_file)
     db = connection.cursor()
 
     sql_instruction = f"SELECT UserPassword FROM Users WHERE UserName == '{entered_user_name}'"
@@ -82,15 +85,15 @@ def check_user_password_in_database(entered_user_name, entered_user_password):
     return False
 
 def delete_user_from_database(user_name):
-    users_db_file = "./Users.db"
+    global db_file
 
-    if not path.exists(users_db_file):
+    if not path.exists(db_file):
         print("Database does not exist")
         return False
     
     sql_instruction = f"DELETE from Users WHERE UserName = ('{user_name}');"
     
-    connection = connect(database=users_db_file)
+    connection = connect(database=db_file)
     db = connection.cursor()
 
     db.execute(sql_instruction)
@@ -99,8 +102,92 @@ def delete_user_from_database(user_name):
     connection.close()
     return True
 
+def create_booking_data(year, day, base_fare, total_spaces, days_for_sale):
+    global db_file
+
+    if not path.exists(db_file):
+        print("Database does not exist")
+        create_database(db_file)
+    
+    sql_instruction = f"SELECT Year, Day FROM BookingData WHERE Year == ({year}) and Day == ({day})"
+
+    connection = connect(database=db_file, timeout=5)
+    db = connection.cursor()
+
+    db.execute(sql_instruction)
+
+    rows = db.fetchall()
+
+    db.close()
+    connection.close()
+    
+    if len(rows) == 0:
+        print("adding")
+        connection = connect(database=db_file, timeout=5)
+        db = connection.cursor()
+        sql_instruction = f"INSERT INTO BookingData(Year, Day, BaseFare, TotalSpaces, DaysForSale) VALUES ({year}, {day},{base_fare}, {total_spaces},{days_for_sale});"
+
+        db.execute(sql_instruction)
+        connection.commit()
+        db.close()
+        connection.close()
+
+def change_booking_data_sold_spaces(year, day, space_sold):
+    global db_file
+
+    if not path.exists(db_file):
+        print("Database does not exist")
+        return False
+    
+    sql_instruction = f"SELECT Year, Day FROM BookingData WHERE Year == ({year}) and Day == ({day})"
+
+    connection = connect(database=db_file, timeout=5)
+    db = connection.cursor()
+
+    db.execute(sql_instruction)
+
+    rows = db.fetchall()
+
+    db.close()
+    connection.close()
+
+    if len(rows) == 1:
+        print("updating")
+        connection = connect(database=db_file, timeout=5)
+        db = connection.cursor()
+        sql_instruction = f"UPDATE BookingData SET SpacesSold = {space_sold} WHERE Year == ({year}) and  Day == ({day})"
+        db.execute(sql_instruction)
+        connection.commit()
+        db.close()
+        connection.close()
+
+def get_booking_data_sold_spaces(year, day):
+    global db_file
+
+    if not path.exists(db_file):
+        print("Database does not exist")
+        return False
+    
+    sql_instruction = f"SELECT Year, Day, BaseFare, TotalSpaces, SpacesSold, DaysForSale FROM BookingData WHERE Year == ({year}) and Day == ({day})"
+
+    connection = connect(database=db_file, timeout=5)
+    db = connection.cursor()
+
+    db.execute(sql_instruction)
+
+    rows = db.fetchall()
+
+    db.close()
+    connection.close()
+
+    return rows
+
+
 if __name__ == "__main__":
     # print(add_user_to_database("tim@user.com","password"))
     # print(check_user_password_in_database("tim@user.com","password"))
-    print(delete_user_from_database("tim@user.com"))
+    #print(delete_user_from_database("tim@user.com"))
+    # create_booking_data(2023,300,100,99,30)
+    change_booking_data_sold_spaces(2023,300,5)
+    #print(get_booking_data_sold_spaces(2023, 300))
 
--- yield_management.py
+++ yield_management.py
@@ -1,4 +1,5 @@
 import math
+from database import *
 
 class Fare_Calculator:
     def __init__(self, base_fare, total_spaces, spaces_sold, days_till_permit, days_for_sale):
@@ -8,8 +9,8 @@ class Fare_Calculator:
         self.days_for_sale = days_for_sale
         self.spaces_sold = spaces_sold
         assert days_till_permit <= days_for_sale, "Can not have days till permit, greater than days for sale"
-        print(f'Base fare: ${base_fare}\nTotal spaces {total_spaces}\nSpaces sold: {spaces_sold}\nSpaces expected to be sold: {self.expected_remaining()}\nDays to go: {days_till_permit}\nDays for sale: {days_for_sale}')
-        
+        assert spaces_sold <= total_spaces, "Can not have more spaces sold than total spaces"
+        print(f'Base fare: ${base_fare}\nTotal spaces: {total_spaces}\nSpaces sold: {spaces_sold}\nSpaces expected to be sold: {self.expected_remaining()}\nDays to go: {days_till_permit}\nDays for sale: {days_for_sale}')
 
     def expected_remaining(self):
         expected_change_per_day = self.total_spaces / self.days_for_sale
@@ -20,6 +21,6 @@ class Fare_Calculator:
         return math.floor(self.base_fare * self.spaces_sold / self.expected_remaining())
     
 if __name__ == "__main__":
-    cal = Fare_Calculator(100,45,100,15,30)
-    print(cal.expected_remaining())
+    datalist = year, day, base_fare, total_spaces, spaces_sold, days_for_sale = get_booking_data_sold_spaces(2023,300)[0]
+    cal = Fare_Calculator(base_fare,total_spaces,spaces_sold,15,days_for_sale)
     print(f"new price ${cal.calculate_fare()}")
