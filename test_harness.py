39a935f948210c1b0bbf5a0c31876c52797b2673
James Boon <61937112+JimmyBoon@users.noreply.github.com>
2023-05-06 20:10:11 +1000

    Update test_harness.py

--- test_harness.py
+++ test_harness.py
@@ -1,4 +1,11 @@
+import requests
+
 class TestHarness:
 
     def test1(self):
         assert 1 == 1
+        
+    def test_index_page(self):
+        url = 'http://127.0.0.1:8080'
+        r = requests.get(url+'/') # Assumses that it has a path of "/"
+        assert r.status_code == 200 # Assumes that it will return a 200 response
